f210239130a89b4678e01b4507b673ad87bcc8a9
dpodolskiy1990 <dpodolskiy1990@gmail.com>
2023-07-30 22:05:51 +0300

    add section 6

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-
 import { Col, Container, Image, Row } from "react-bootstrap";
 import Products from "./Products";
 import { useSelector } from "react-redux";
@@ -19,84 +18,100 @@ import KOMBUCHArIGHT from "../img/KOMBUCHArIGHT.png";
 import HomeHead from "../pages/F2/HomeHead/HomeHead";
 import { ROUTES } from "../utils/routes";
 import { Link } from "react-router-dom";
+import Modern from "../pages/F2/Modern/Modern";
 
 function Home() {
-  const { list } = useSelector(({products}) => products) ;
-  
-  
-return (
-  <div className={styles.home}>
-  <HomeHead/>
-  <AnimatedLine/>
-    <div className={styles.pics} justifyContent="between">
-      <Image src={Group_1} rounded className={styles.picBotton}/>
-      <Image src={Group_2} rounded className={styles.leaf}/>
-    </div>
-  
-    <div className={styles.section}>
-      <h3 className={styles.section_title}>What is kombucha</h3>
-      <p className={styles.section_text}>Kombucha is a naturally fermented beverage made from tea and sugar. Fermentation results in a special culture of probiotics and enzymes.</p>
-      <div className={styles.position_img}>
+  const { list } = useSelector(({ products }) => products);
 
-      <Image src={Rectangle} rounded className={styles.section_Rectangle}/>
+  return (
+    <div className={styles.home}>
+      <HomeHead />
+      <AnimatedLine />
+      <div className={styles.pics} justifyContent="between">
+        <Image src={Group_1} rounded className={styles.picBotton} />
+        <Image src={Group_2} rounded className={styles.leaf} />
       </div>
-      <p className={styles.section_textt}>Contains antioxidants that your body will benefit from helps to keep you energized and <span className={styles.pink}>fresh after a busy day.</span></p>
-    </div>
-  
-    <Container fluid className={styles.cont_imgs}>
-      <Row className={styles.section_imgs}>
-        <Col>
-          <Image className={styles.section_kombucha} src={KOMBUCHA}/>
-          <div className={styles.section_benefits}>
-            <p className={styles.section_benefitsP}>Benefits</p>
-            <img src={Arrow} alt="" />
-          </div>
-        </Col>
-        <Col className={styles.section_pngwing}>
-          <Image src={pngwing} />
-        </Col>
 
-      </Row>
-    </Container>
-
-  <div className={styles.sectionFluent}>
-      <h3 className={styles.section_title}>Feel charge of health</h3>
-      <Image src={Bottle} rounded className={styles.section_FluidImg}/>
-      <h3 className={styles.section_titleSec}>That kambucha gives to you</h3>
-  </div>
-
-  <div>
-    <Image src={Flowers} className={styles.section_imgLeft}/>
-    <p className={styles.imgLeft_title}>Kambucha makes your holiday</p>
-    <p className={styles.imgLeft_text}>Embrace the Vibrant Holiday Experience with Kambucha's Unmatched Taste, Energy, and Healthy Lifestyle Benefits!</p>
-  </div>
-
-  <div className={styles.Taste}>TASTE GOOD</div>
-
-  <Container fluid className={styles.Botla}>
-    <Image src={Botla} className={styles.Botla_img}/>
-  </Container>
-
-  <div className={styles.sectionBlue}>
-    <Image src={Orange} className={styles.imgBlue_little}/>
-    <Image src={Gratefruit} className={styles.imgBlue_big}/>
-
-   <p className={styles.sectionBlue_p1}>Revitalize</p>
-    <p className={styles.sectionBlue_p2}>Your<br/> Body</p>
-    <Link to={ROUTES.SHOP} className={styles.sectionBlue_p1}>with Kombucha</Link>
-    <p className={styles.sectionBlue_p3}>AND Choose</p>
-    <div>
-        <Image className={styles.spot} src={KOMBUCHArIGHT}/>
-        <div className={styles.spot_sec}>
-          <p className={styles.spot_p}>Future<br/> Favorite<br/> Drink</p>
-          <img className={styles.spot_arrow} src={Arrow} alt="" />
+      <div className={styles.section}>
+        <h3 className={styles.section_title}>What is kombucha</h3>
+        <p className={styles.section_text}>
+          Kombucha is a naturally fermented beverage made from tea and sugar.
+          Fermentation results in a special culture of probiotics and enzymes.
+        </p>
+        <div className={styles.position_img}>
+          <Image src={Rectangle} rounded className={styles.section_Rectangle} />
         </div>
+        <p className={styles.section_textt}>
+          Contains antioxidants that your body will benefit from helps to keep
+          you energized and{" "}
+          <span className={styles.pink}>fresh after a busy day.</span>
+        </p>
+      </div>
+
+      <Container fluid className={styles.cont_imgs}>
+        <Row className={styles.section_imgs}>
+          <Col>
+            <Image className={styles.section_kombucha} src={KOMBUCHA} />
+            <div className={styles.section_benefits}>
+              <p className={styles.section_benefitsP}>Benefits</p>
+              <img src={Arrow} alt="" />
+            </div>
+          </Col>
+          <Col className={styles.section_pngwing}>
+            <Image src={pngwing} />
+          </Col>
+        </Row>
+      </Container>
+
+      <div className={styles.sectionFluent}>
+        <h3 className={styles.section_title}>Feel charge of health</h3>
+        <Image src={Bottle} rounded className={styles.section_FluidImg} />
+        <h3 className={styles.section_titleSec}>That kambucha gives to you</h3>
+      </div>
+
+      <div>
+        <Image src={Flowers} className={styles.section_imgLeft} />
+        <p className={styles.imgLeft_title}>Kambucha makes your holiday</p>
+        <p className={styles.imgLeft_text}>
+          Embrace the Vibrant Holiday Experience with Kambucha's Unmatched
+          Taste, Energy, and Healthy Lifestyle Benefits!
+        </p>
+      </div>
+
+      <div className={styles.Taste}>TASTE GOOD</div>
+      <Modern />
+      <Container fluid className={styles.Botla}>
+        <Image src={Botla} className={styles.Botla_img} />
+      </Container>
+
+      <div className={styles.sectionBlue}>
+        <Image src={Orange} className={styles.imgBlue_little} />
+        <Image src={Gratefruit} className={styles.imgBlue_big} />
+
+        <p className={styles.sectionBlue_p1}>Revitalize</p>
+        <p className={styles.sectionBlue_p2}>
+          Your
+          <br /> Body
+        </p>
+        <Link to={ROUTES.SHOP} className={styles.sectionBlue_p1}>
+          with Kombucha
+        </Link>
+        <p className={styles.sectionBlue_p3}>AND Choose</p>
+        <div>
+          <Image className={styles.spot} src={KOMBUCHArIGHT} />
+          <div className={styles.spot_sec}>
+            <p className={styles.spot_p}>
+              Future
+              <br /> Favorite
+              <br /> Drink
+            </p>
+            <img className={styles.spot_arrow} src={Arrow} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <Products products={list} amount={4} />
     </div>
-  </div>
-
-  <Products products={list} amount={4} />
-</div>
-
-    )
-    }
+  );
+}
 export default Home;
---
+++ src/pages/F2/Modern/Modern.jsx
@@ -0,0 +1,66 @@
+import "./Modern.scss";
+
+export default (Modern) => {
+  return (
+    <div className="Modern">
+      <h2>Why you should try OUR KOMBUCHA</h2>
+      <div className="Modern_img">
+        <img src="./img/modern_combucha.png" alt="modern" />
+      </div>
+      <h3>A modern taste from childhood</h3>
+      <p>
+        Kombucha is a delicious drink with a unique flavor unlike anything else
+        on the beverage aisle. Try out different varieties to find your sweet
+        spot. With hundreds of craft brewers, there’s a kombucha for everyone.
+      </p>
+      <div className="Triple_bottle">
+        <img src="./img/Linked_Path_Group.png" alt="Linked_Path_Group" />
+        <img src="./img/Triple_bottle.jpg" alt="Triple_bottle" />
+      </div>
+      <div className="Unleash_the_Benefits">
+        <div>
+          <img src="./img/unleash_img.jpg" alt="unleash_img" />
+        </div>
+        <div>
+          <h5>
+            <img src="./img/Unleash_line.png" alt="Unleash_line" /> Unleash the
+            Benefits
+          </h5>
+          <p>
+            Our Kambucha promotes enhanced digestion, antibacterial protection,
+            and Iimmune system support.
+          </p>
+        </div>
+      </div>
+      <div className="Crafted_with">
+        <img src="./img/Crafted_with_img.jpg" alt="Crafted_with_img" />
+        <div className="Crafted_with_head">
+          <h3>
+            <img src="./img/crafted_splash.png" alt="crafted_splash" /> Crafted
+            with сare an pasion
+          </h3>
+          <p>
+            We meticulously choose the finest ingredients to ensure the
+            production of our exceptional kambucha, upholding our commitment to
+            quality without compromise.
+          </p>
+        </div>
+        <div className="Crafted_with_bottom">
+          <div>
+            <img src="./img/Quality.jpg" alt="Quality" />
+          </div>
+          <div>
+            <h5>
+              <img src="./img/Quality_splash.jpg" alt="Quality_splash" />{" "}
+              Quality is Key
+            </h5>
+            <p>
+              Our relentless focus on detail guarantees the exceptional quality
+              of our product at every step of the production process.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
